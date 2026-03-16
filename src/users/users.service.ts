ff266fc84b7b6146fedad80ce44eea56023d7675
Pedro Henrique <pedrohenriqueb366@gmail.com>
2026-03-15 22:36:24 -0300

    feat: create users database

---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const users = await this.prismaService.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: createUserDto.password,
+      },
+    });
+
+    return users;
+  }
+}
